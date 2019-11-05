e6b2c4151e1585803b2d5bfa951c5d8235f9839f
michi-tech <michi.swd@gmail.com>
2019-11-05 12:12:26 +0900

    replaced the message in sample.

--- sample.rb
+++ sample.rb
@@ -1,5 +1,25 @@
-def hello
-  p "hello, GitHub."
-end
+class Article
 
-hello
+  def initialize(author, title, content)
+    @author = author
+    @title = title
+    @content = content
+  end
+
+  def author
+    @author
+  end
+
+  def title
+    @title
+  end
+
+  def content
+    @content
+  end
+
+end
+article = Article.new("阿部", "Rubyの素晴らしさについて", "Awesome Ruby!")
+puts "著者: #{article.author}"
+puts "タイトル: #{article.title}"
+puts "本文: #{article.content}"
